3d02ef84bd7e937a99aad8ee5176086dbc96c791
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-15 22:27:14 +0300

    Eslint configuration is set

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,21 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    node: true,
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    'prettier'
   ],
-  parserOptions: {
-    ecmaVersion: 'latest'
-  }
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'never'],
+    'prefer-const': ['error', {
+      destructuring: 'all',
+      ignoreReadBeforeAssign: true,
+    }]
+  },
 }
