36cab06c6dd4f40895944cbff1e4c39b70f1cff2
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-16 16:00:50 +0300

    With the help of axios, comics array was obtained from the api and written to the store

--- src/components/comics/ComicsBase.vue
+++ src/components/comics/ComicsBase.vue
@@ -6,6 +6,33 @@
 
 <script setup lang="ts">
 import CardBase from '@/components/comics/components/CardBase.vue'
+import axios from 'axios'
+import md5 from 'md5'
+import { useStore } from 'vuex'
+import { onBeforeMount, ref } from 'vue'
+
+const store = useStore()
+
+const publicKey = '752836521b1549cc8a96031091242bac'
+const privateKey = '1bf491dcde19ebf703fb09c746ca63e9e17685df'
+const baseURL = 'https://gateway.marvel.com/v1/public'
+
+onBeforeMount(() => {
+  axios.get(`${baseURL}/characters`, {
+    params: {
+      apikey: publicKey,
+      ts: Date.now(),
+      hash: generateHash(Date.now(), privateKey, publicKey)
+    }
+  }).then(response => {
+    store.commit('setComicsList', response.data.data.results)
+  }).catch(error => {
+    console.error('error', error)
+  })
+})
+function generateHash(timestamp, privateKey, publicKey) {
+  return md5(timestamp + privateKey + publicKey)
+}
 
 </script>
 
