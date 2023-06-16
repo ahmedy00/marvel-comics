df78b065fb73d03c846b00a22a484291981304fe
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-16 15:59:46 +0300

    Defined state, mutations and getters

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,1 +1,20 @@
 import { createStore } from 'vuex'
+
+const store = createStore({
+    state: {
+        // FIXME: Need better type declaration
+        comicsList: [] as any
+    },
+    mutations: {
+        setComicsList (state, list) {
+            state.comicsList = list
+        }
+    },
+    getters: {
+        getComicsList (state) {
+            return state.comicsList
+        }
+    }
+})
+
+export default store
