6752657e4b44d3ac2fad9013dede7025944aa89d
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-17 01:11:20 +0300

    Favourite functions are defined

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,30 @@ const store = createStore({
     plugins: [createPersistedState()],
     state: {
         // FIXME: Need better type declaration
-        comicsList: [] as any
+        comicsList: [] as any,
+        favouriteComics: [] as string[]
     },
     mutations: {
-        setComicsList (state, list) {
+        // FIXME: Need better type declaration
+        setComicsList (state, list: any) {
             state.comicsList = list
+        },
+        addToFavouriteComic (state, comicId: string) {
+            state.favouriteComics.push(comicId)
+        },
+        removeFromFavourites (state, comicId: string) {
+            const index = state.favouriteComics.indexOf(comicId)
+            if (index > -1) {
+                state.favouriteComics.splice(index, 1)
+            }
         }
     },
     getters: {
         getComicsList (state) {
             return state.comicsList
+        },
+        getFavouriteComics (state) {
+            return state.favouriteComics
         }
     }
 })
