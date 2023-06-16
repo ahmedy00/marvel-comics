55fd0ed703ac0f631c64577363c46e69ca8de464
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-16 14:39:41 +0300

    router settings done for header buttons

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,28 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import ComicsBase from '@/components/comics/ComicsBase.vue'
+import FavouritesBase from '@/components/favourites/FavouritesBase.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      component: HomeView
+      name: 'Home',
+      component: HomeView,
+      redirect: 'comics',
+      children: [
+        {
+          path: 'comics',
+          name: 'Comics',
+          component: ComicsBase
+        },
+        {
+          path: 'favourites',
+          name: 'Favourites',
+          component: FavouritesBase
+        }
+      ]
     }
   ]
 })
