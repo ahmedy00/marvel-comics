78dba82d240e3fb4a310ec58fe0af6e744edebd2
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-16 15:12:03 +0300

    Created comics components

--- src/components/comics/ComicsBase.vue
+++ src/components/comics/ComicsBase.vue
@@ -1,11 +1,21 @@
 <template>
-  <div>comics</div>
+  <div class="comics-wrapper">
+    <CardBase/>
+  </div>
 </template>
 
 <script setup lang="ts">
+import CardBase from '@/components/comics/components/CardBase.vue'
 
 </script>
 
 <style scoped>
+
+.comics-wrapper {
+  position: absolute;
+  top: 4rem;
+  left: 0;
+  width: 100%;
+}
 
 </style>
---
+++ src/components/comics/components/CardBase.vue
@@ -0,0 +1,12 @@
+<template>
+  <Card/>
+</template>
+
+<script setup lang="ts">
+import Card from '@/components/comics/components/Card.vue'
+
+</script>
+
+<style scoped>
+
+</style>
