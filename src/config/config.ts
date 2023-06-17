1739530f6f8f9230492b2f38ebbcf97cef7427bf
Ahmet <dede_dede_ahmet@hotmail.com>
2023-06-17 10:10:01 +0300

    Added types to config file

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,9 +1,15 @@
 import md5 from 'md5'
+
+type ConfigType = {
+    publicKey: string
+    privateKey: string
+    baseURL: string
+}
 export const config = {
     publicKey: '752836521b1549cc8a96031091242bac',
     privateKey: '1bf491dcde19ebf703fb09c746ca63e9e17685df',
     baseURL: 'https://gateway.marvel.com/v1/public'
-}
+} as ConfigType
 
 export function generateHash(timestamp: number, privateKey: string, publicKey: string) {
   return md5(timestamp + privateKey + publicKey)
