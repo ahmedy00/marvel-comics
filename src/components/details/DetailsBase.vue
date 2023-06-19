42fa6f76a82cd63cdb6be2183d2c05362b2ec74e
Ahmet Dede <ahmet.dede@turboard.com>
2023-06-19 12:53:46 +0300

    Created a component name DetailBase and found related comic with compare router param id and store comics list

---
+++ src/components/details/DetailsBase.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="detail-wrapper">
+    <Detail :comic="comic"/>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Detail from '@/components/details/components/Detail.vue'
+import { useRoute } from 'vue-router'
+import { useStore } from 'vuex'
+import type { ComicsType } from '@/types'
+import { computed } from 'vue'
+
+const route = useRoute()
+
+const store = useStore()
+
+
+const comic = computed(() => {
+  return store.getters.getComicsList.find((comic: ComicsType) => {
+    return comic.id.toString() === route.params.id
+  })
+})
+
+</script>
+
+<style scoped>
+
+.detail-wrapper {
+  position: absolute;
+  top: 5.5rem;
+  left: 0;
+  width: 100%;
+}
+
+</style>
