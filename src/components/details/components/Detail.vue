4dbe70eec59dc3d83aafe4edb65a8da3ee36c836
Ahmet Dede <ahmet.dede@turboard.com>
2023-06-19 12:54:17 +0300

    Created template for detail design

---
+++ src/components/details/components/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ comic }}
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps(['comic'])
+
+console.log('comic', props.comic)
+
+</script>
+
+<style scoped>
+
+</style>
