eb93da7bdb20f86d363d8af3ff2e38e4763a4a35
Ahmet Dede <ahmet.dede@turboard.com>
2023-06-19 12:55:44 +0300

    Route settings done for comic details

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import ComicsBase from '@/components/comics/ComicsBase.vue'
 import FavouritesBase from '@/components/favourites/FavouritesBase.vue'
+import DetailsBase from '@/components/details/DetailsBase.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,12 @@ const router = createRouter({
           path: 'favourites',
           name: 'Favourites',
           component: FavouritesBase
+        },
+        {
+          path:'comic/:id',
+          name: 'ComicDetail',
+          component: DetailsBase,
+          props: true
         }
       ]
     }
