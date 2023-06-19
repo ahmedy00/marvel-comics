340063e48a1cd087909e00d4a5a5ca4c7b64c67e
Ahmet Dede <ahmet.dede@turboard.com>
2023-06-19 13:45:48 +0300

    Added an image below header

---
+++ src/components/header/components/HeaderImage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="header-image mt-4">
+    <img style="width: 95vw; border-radius: 12px" src="https://pbs.twimg.com/media/CQRG7KvUwAAKmc2.jpg" alt="marvel-heroes">
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+.header-image::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1;
+}
+
+
+
+</style>
